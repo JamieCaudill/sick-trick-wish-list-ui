f2bb5b11d88d836fdcb49ff0b21a8cdd92229347
Jamie Caudill <j.caudill7177@gmail.com>
2023-07-24 09:18:12 -0700

    feat: Add select for obstacle

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -16,6 +16,15 @@ export default function Form({tricks}) {
         <option value="Switch">Switch</option>
       </select>
       <input type="text" placeholder="Name of Trick" onChange={(e) => setTrickName(e.target.value)} />
+      <select id="obstacle" value={obstacle} onChange={(e) => setObstacle(e.target.value)}>
+        <option value="">Choose your Obstacle</option>
+        <option value="flatground">Flatground</option>
+        <option value="ledge">Ledge</option>
+        <option value="rail">Rail</option>
+        <option value="stairs">Stairs</option>
+        <option value="pool">Pool</option>
+      </select>
+      
     </form>
   )
 }

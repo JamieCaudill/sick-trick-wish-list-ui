12df0b991f545a7f94bfcf71bf678c0b39f32252
Jamie Caudill <j.caudill7177@gmail.com>
2023-07-24 08:23:44 -0700

    feat: Import getTricks to App.js

--- src/api-calls/api-calls.js
+++ src/api-calls/api-calls.js
@@ -2,3 +2,10 @@
 
 // fetch all tricks on page load and get them to display on the DOM
 
+function getTricks() {
+  fetch('http://localhost:3001/api/v1/tricks')
+  .then(response => response.json())
+  .then(data => console.log(data))
+}
+
+export { getTricks }
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,15 @@
 import './App.css';
 import React from 'react';
+import { useState, useEffect } from 'react';
 
 
+import { getTricks } from '../../api-calls/api-calls';
 
 function App() {
+  useEffect(() => {
+    getTricks()
+  },[])
+
   return (
     <div className="App">
       <h1>Sick Trick Wish List</h1>

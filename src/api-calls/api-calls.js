0e30db79623a1c359cad27cf0f9e602fbb2d5d7e
Jamie Caudill <j.caudill7177@gmail.com>
2023-07-24 08:29:06 -0700

    feat: Add error handling to fetch

--- src/api-calls/api-calls.js
+++ src/api-calls/api-calls.js
@@ -3,9 +3,14 @@
 // fetch all tricks on page load and get them to display on the DOM
 
 function getTricks() {
-  fetch('http://localhost:3001/api/v1/tricks')
-  .then(response => response.json())
-  .then(data => console.log(data))
+  fetch('http://localhost:3001/api/v1/trcks')
+  .then(response => {
+    console.log(response)
+    if (!response.ok) {
+      throw new Error(`${response.status}: ${response.statusText}`)
+    }
+  return response.json();
+  })
 }
 
 export { getTricks }
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,8 +6,15 @@ import { useState, useEffect } from 'react';
 import { getTricks } from '../../api-calls/api-calls';
 
 function App() {
+  const [error, setError] = useState();
+
   useEffect(() => {
     getTricks()
+      .then(data => {
+        console.log(data)
+      })
+      .catch(error => {console.log(error)})
+      // print error on DOM
   },[])
 
   return (
